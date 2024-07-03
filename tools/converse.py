5d683a4db4f2a79466402be0a73dd359cfdfcbf5
Drixler Angelo <drixler97@gmail.com>
2024-07-03 23:28:43 +0800

    style: change the layout

--- tools/converse.py
+++ tools/converse.py
@@ -1,22 +1,40 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores.faiss import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_community.llms.ollama import Ollama
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain_openai import ChatOpenAI
+from langchain.chains.conversational_retrieval.base \
+    import ConversationalRetrievalChain
 from core import settings
 from typing import List, Dict, Tuple
 
+
 def run(query: str, history: List[Tuple[str, str]] = []) -> Dict:
-    print(history)
     if settings.LLM_TYPE == 'ollama':
-        embeddings = HuggingFaceEmbeddings(model_kwargs={'device': 'cuda'})
+        embeddings = HuggingFaceEmbeddings(
+            model_kwargs={'device': 'cuda'},
+            cache_folder='./.cache'
+        )
         chat_model = Ollama(
             model=settings.LLM_NAME,
             base_url=settings.LLM_URL,
             temperature=0,
         )
+    elif settings.LLM_TYPE == 'openai':
+        embeddings = HuggingFaceEmbeddings(
+            model_kwargs={'device': 'cpu'},
+            cache_folder='./.cache'
+        )
+        chat_model = ChatOpenAI(
+            openai_api_key=settings.LLM_KEY,
+            model_name=settings.LLM_NAME,
+            temperature=0,
+        )
     else:
-        embeddings = HuggingFaceEmbeddings(model_kwargs={'device': 'cpu'})
+        embeddings = HuggingFaceEmbeddings(
+            model_kwargs={'device': 'cpu'},
+            cache_folder='./.cache'
+        )
         chat_model = ChatGoogleGenerativeAI(
             google_api_key=settings.LLM_KEY,
             model=settings.LLM_NAME,
@@ -43,10 +61,13 @@ def run(query: str, history: List[Tuple[str, str]] = []) -> Dict:
         "chat_history": history,
     })
 
+    sources = [doc.metadata['source'] for doc in result['source_documents']]
+    sources = list(set(sources))
+
     return {
         'question': query,
         'answer': result['answer'],
         'metadata': {
-            'sources': list(set([doc.metadata['source'] for doc in result['source_documents']])),
+            'sources': sources,
         }
     }
